bc167a3ab8b80fcfa970ef609dffeba6ca662a9c
kshantigreene <kshanti@gmail.com>
2024-10-07 15:44:38 -0400

    Commiting merge. Added some localStorage commands

--- dom.js
+++ dom.js
@@ -1,10 +1,18 @@
-/*** Change the color of some text */
-function changeButton(){
-    document.getElementById('jsstyled').style.color="purple"
+function testLocal(){
+    localStorage.setItem("test","test1")
 }
 
-/*** Initialize the page characteristics */
+/*** Change the color of some text */
+function changeButton(){
+    location.href="second.html"
+    //document.getElementById('jsstyled').style.color="purple"
+}
+
+/***
+ * Initialize page characteristics
+ */
 function initPage(){
+    testLocal()
     var content=document.getElementById('content')
     content.style.backgroundColor="lightGray"
     content.style.textAlign="center"
@@ -17,38 +25,48 @@ function initPage(){
     image.style.margin="AUTO"
     image.style.width="80%"
     image.style.textAlign="center"
+    localStorage.setItem('LastLanguage','python')
+    console.log(localStorage.getItem('LastLanguage'))
+
 }
 
-/***
- * Toggle which divs are visible
+/**
+ * Switch between displaying content and image divs
  */
 function switchContent(){
     
+    //grab two components.. div called content and div called image
     var content=document.getElementById('content')
     var image=document.getElementById('image')
     console.log("switchContent "+content.style.display)
     
     if (content.style.display == "none"){
-        //if content is not showing, show it and hide image
+    //if content is not showing, switch to showing content (display=block)
+
         image.style.display="none"
         content.style.display="block"
         
-    }else{
-        //if content is showing, hide it and show image
+    }else{ 
+        //if content is showing, switch to showing image
         content.style.display="none"
         image.style.display="block"
     }
+    
 }
 
 /***
  * Change text of text area based on language choice
  */
 function selectLanguage(choice){
+    //get the component by ID
     var text=document.getElementById('code')
+    console.log(text.value)
     var codeText=""
+    //set the new text
     switch(choice){
         case 'java':
             codeText="System.out.println('Hello World')"
+            break
         case 'python':
             codeText="print('Hello World')"
             break
@@ -65,7 +83,12 @@ function selectLanguage(choice){
             codeText="Hello World"
 
     }
-    //set the text of the text area
+    //change the text area's text
     text.value=codeText
 
 }
+
+function getLocal(){
+    console.log("Last cuisine was: "+localStorage.getItem("cuisine"))
+}
+
